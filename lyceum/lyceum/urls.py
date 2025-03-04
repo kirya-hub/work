769b8cf63ac1267d41cd4d1af421c5ef76769de1
Kirill <fizka67@bk.ru>
2025-03-04 21:51:11 +0300

    исправления

--- lyceum/lyceum/urls.py
+++ lyceum/lyceum/urls.py
@@ -1,19 +1,17 @@
 from django.conf import settings
-import django.contrib.admin
-import django.urls
+from django.contrib import admin
+from django.urls import include, path
 
 import about.urls
 import catalog.urls
 import homepage.urls
 
-from django.urls import include, path
-
 
 urlpatterns = [
-    django.urls.path("", django.urls.include(homepage.urls)),
-    django.urls.path("about/", django.urls.include(about.urls)),
-    django.urls.path("catalog/", django.urls.include(catalog.urls)),
-    django.urls.path("admin/", django.contrib.admin.site.urls),
+    path("", include(homepage.urls)),
+    path("about/", include(about.urls)),
+    path("catalog/", include(catalog.urls)),
+    path("admin/", admin.site.urls),
 ]
 
 if settings.DEBUG:
